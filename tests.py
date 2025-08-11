cdacbf374871f02a3f49a2c023be74850deca016
tseeberCTR <tseeber.CTR@uncomn.com>
2025-08-11 10:32:35 -0700

    Run Python Capability Added

--- tests.py
+++ tests.py
@@ -1,13 +1,19 @@
-from functions.write_file import write_file
+from functions.run_python import run_python_file
 
 
 def test():
-    response=write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
-    print(response)
-    response=write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
-    print(response)
-    response=write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
-    print(response)
+    result = run_python_file("calculator", "main.py", "3 + 5")
+    print(result)
+
+    result = run_python_file("calculator", "tests.py")
+    print(result)
+
+    result = run_python_file("calculator", "../main.py")
+    print(result)
+
+    result = run_python_file("calculator", "nonexistent.py")
+    print(result)
+
     
 if __name__ == "__main__":
     test()
